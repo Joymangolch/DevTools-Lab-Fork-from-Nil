3703978c49cedfb922222c0b7373d735db59718a
nilaelang <117297100+nilaelang@users.noreply.github.com>
2023-12-13 15:19:23 +0530

    Update prod.c

---
+++ prod.c
@@ -0,0 +1,11 @@
+//program to find the Product of two numbers
+#include<stdio.h>
+int main()
+{
+int a,b,prod;
+printf("Enter Two Numbers\n");
+scanf("\n%d\n%d",&a,&b);
+prod=a*b;
+printf("\nProduct  is =%d",prod);
+return 0;
+}
