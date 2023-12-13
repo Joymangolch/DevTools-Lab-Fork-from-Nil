96926db369c869529581d5b58a170bdc9cea1190
nilaelang <117297100+nilaelang@users.noreply.github.com>
2023-12-13 15:16:33 +0530

    Update diff.c

---
+++ diff.c
@@ -0,0 +1,14 @@
+//program to find the Difference of two numbers
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,dif;
+clrscr();
+printf("Enter Two Numbers\n");
+scanf("\n%d\n%d",&a,&b);
+dif=a-b;
+printf("\nDifference is =%d",dif);
+getch();
+return 0;
+}
