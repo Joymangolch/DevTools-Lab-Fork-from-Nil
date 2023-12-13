b4cea44a5d152e044434b3bb6261af979bb5b248
nilaelang <117297100+nilaelang@users.noreply.github.com>
2023-12-13 15:18:08 +0530

    Update add.c

---
+++ add.c
@@ -0,0 +1,13 @@
+//program to find the Sum of two numbers
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+
+printf("Enter Two Numbers\n");
+scanf("\n%d\n%d",&a,&b);
+sum=a+b;
+printf("\nSum is =%d",dif);
+
+return 0;
+}
